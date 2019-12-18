3dff2c40657ea1edbd58f6f865060736773ef4a1
unknown <2541601705@qq.com>
2019-12-18 13:38:06 +0800

    井上小百合

---
+++ back-end/src/main/java/com/example/demo/Controller/ZhongzhuanController.java
@@ -0,0 +1,61 @@
+package com.example.demo.Controller;
+
+
+import com.example.demo.Entity.Expert;
+import com.example.demo.Entity.Resources;
+import com.example.demo.Entity.ZhongZhuan;
+import com.example.demo.Repository.ExpertRepository;
+import com.example.demo.Repository.ResourcesRepository;
+import com.example.demo.Repository.ZhongzhuanRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class ZhongzhuanController {
+
+    @Autowired
+    private ZhongzhuanRepo zhongzhuanRepo;
+    @Autowired
+    private ExpertRepository expertRepository;
+    @Autowired
+    private ResourcesRepository resourcesRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @ResponseBody
+    @GetMapping("/zhongzhuan")
+    public String zhongzhuan()
+    {
+        List<ZhongZhuan> zhongZhuans=zhongzhuanRepo.findAll();
+
+
+        for(int i=0;i<zhongZhuans.size();i++)
+        {
+            Expert expert=new Expert();
+            expert.setRealName(zhongZhuans.get(i).getName());
+            expert=expertRepository.save(expert);
+
+            for(int j=0;j<zhongZhuans.get(i).getPapers().size();j++)
+            {
+                Resources resources=new Resources();
+                resources.setUserId(expert.getId());
+                resources.setTitle(zhongZhuans.get(i).getPapers().get(j).getTitle());
+                resources.setTime(zhongZhuans.get(i).getPapers().get(j).getYear());
+                resourcesRepository.save(resources);
+
+            }
+
+
+        }
+
+        return "success";
+
+    }
+
+
+}
---
+++ back-end/src/main/java/com/example/demo/Entity/Paper.java
@@ -0,0 +1,22 @@
+package com.example.demo.Entity;
+
+public class Paper {
+    public String title;
+    public String year;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
